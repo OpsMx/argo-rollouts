6a9ff737e34ccd851978a4e622c1131202ec1a58
shraddhaopsmx <shraddha.singh@opsmx.io>
2022-09-30 16:31:22 +0530

    added a log line

--- metricproviders/opsmx/opsmx.go
+++ metricproviders/opsmx/opsmx.go
@@ -454,6 +454,7 @@ func (p *Provider) Run(run *v1alpha1.AnalysisRun, metric v1alpha1.Metric) v1alph
 	}
 
 	log.Infof("Before make request")
+	log.Infof("%s", string(buffer))
 	data, err := makeRequest(p.client, "POST", canaryurl, string(buffer), configMapData["user"])
 	if err != nil {
 		return metricutil.MarkMeasurementError(newMeasurement, err)
