68de9a010379c426437ba69654e5340d76c0c700
genos1998 <shaikhjunaid1998@gmail.com>
2022-11-03 04:31:34 +0530

    Support for differentiating UI temp and gitops one
    Signed-off-by: genos1998 <shaikhjunaid1998@gmail.com>

--- metricproviders/opsmx/opsmx.go
+++ metricproviders/opsmx/opsmx.go
@@ -463,7 +463,7 @@ func (p *Provider) Run(run *v1alpha1.AnalysisRun, metric v1alpha1.Metric) v1alph
 				deployment.Baseline.Log[serviceName]["template"] = tempName
 				deployment.Canary.Log[serviceName]["template"] = tempName
 
-				if metric.Provider.OPSMX.GitOPS && templateData[tempName] != "" {
+				if metric.Provider.OPSMX.GitOPS && templateData[tempName] != "" && item.LogTemplateVersion == "" {
 					deployment.Baseline.Log[serviceName]["templateSha1"] = templateData[tempName]
 					deployment.Canary.Log[serviceName]["templateSha1"] = templateData[tempName]
 				}
@@ -515,7 +515,7 @@ func (p *Provider) Run(run *v1alpha1.AnalysisRun, metric v1alpha1.Metric) v1alph
 				//Add templateName
 				deployment.Baseline.Metric[serviceName]["template"] = tempName
 				deployment.Canary.Metric[serviceName]["template"] = tempName
-				if metric.Provider.OPSMX.GitOPS && templateData[tempName] != "" {
+				if metric.Provider.OPSMX.GitOPS && templateData[tempName] != "" && item.MetricTemplateVersion == "" {
 					deployment.Baseline.Metric[serviceName]["templateSha1"] = templateData[tempName]
 					deployment.Canary.Metric[serviceName]["templateSha1"] = templateData[tempName]
 				}
