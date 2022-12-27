674cf70258a5c18927bf1e13a527c8dd9fc9cca6
Veera Venkata Satyanarayana Telu <satya@opsmx.io>
2022-12-27 20:35:14 +0530

    feat: OP-18788-supporting multiple jobs analysis (#34)

--- ui/src/app/components/rollout/reports.tsx
+++ ui/src/app/components/rollout/reports.tsx
@@ -7,8 +7,11 @@ import '../pods/pods.scss';
 export const ReportsWidget = (props: {  clickback: any; reportsInput: {}}) => {
     const [getURL, setURL] = React.useState('');
     const [analysisName, setAnalysisName] = React.useState('');
-    const [validUrl, setValidUrl] = React.useState(true);
+    const [validUrl, setValidUrl] = React.useState(false);
     const [loading, setLoading] = React.useState(true);
+    let conditionArray: any[] = [];
+    let jobsList: any[] = [];
+    let lastIterationJob = 0;
     const isValidUrl = (props: string) => {
       try {
         new URL(props);
@@ -18,7 +21,6 @@ export const ReportsWidget = (props: {  clickback: any; reportsInput: {}}) => {
       }
     }
     const LoadApiCalls = (props: any) => {
-      // console.log('1stapi',props);
         setLoading(true);
         setAnalysisName(props?.reportsInput?.analysisName);
         let url2 = '/api/v1/applications/' + props.reportsInput.appName + '/resource?name=' + props.reportsInput.resourceName + '&appNamespace=' + props.reportsInput.nameSpace + '&namespace=' + props.reportsInput.nameSpace + '&resourceName=' + props.reportsInput.resourceName + '&version=' + props.reportsInput.version + '&kind=AnalysisRun&group=argoproj.io';
@@ -29,10 +31,16 @@ export const ReportsWidget = (props: {  clickback: any; reportsInput: {}}) => {
           .then((data: any) => {
             if (data.manifest.includes('job-name')) {
               let b = JSON.parse(data.manifest);
-              // console.log(b);
-              if (b.status?.metricResults[b.status.metricResults.length - 1]?.measurements[b.status.metricResults.length - 1]?.metadata['job-name']) {
-                fetchEndpointURL(props.reportsInput.appName, props.reportsInput.resourceName, props.reportsInput.nameSpace, props.reportsInput.version, b.status?.metricResults[b.status.metricResults.length - 1]?.measurements[b.status.metricResults.length - 1]?.metadata['job-name']);
-              }
+              const newJobs: any[] = [];
+              const mResults = b.status?.metricResults;
+              mResults.forEach((element: any, index: number) => {
+                newJobs.push(element.measurements[0]?.metadata['job-name']);
+                if (b.status?.metricResults[index]?.measurements[0]?.metadata['job-name']) {
+                  fetchEndpointURL(props.reportsInput.appName, props.reportsInput.resourceName, props.reportsInput.nameSpace, props.reportsInput.version, b.status?.metricResults[index]?.measurements[0]?.metadata['job-name'], index);
+                }
+                jobsList = newJobs;
+                lastIterationJob = newJobs.length - 1;
+              });
             }else{
               setValidUrl(false);
               setLoading(false);
@@ -43,72 +51,46 @@ export const ReportsWidget = (props: {  clickback: any; reportsInput: {}}) => {
           });
       };
 
-    const fetchEndpointURL = (applicationName: String, resouceName: String, nameSpace: String, version: String, jobName: String) => {
+    const fetchEndpointURL = (applicationName: String, resouceName: String, nameSpace: String, version: String, jobName: String, index: number) => {
         let url3 = '/api/v1/applications/' + applicationName + '/resource?name=' + jobName + '&appNamespace=' + nameSpace + '&namespace=' + nameSpace + '&resourceName=' + jobName + '&version=v1&kind=Job&group=batch'
         fetch(url3)
           .then(response => {
             return response.json()
           })
           .then((data: any) => {
-            if (data.manifest.includes('message')) {
-              let a = JSON.parse(data.manifest);
-              // console.log(a);
-              var indexValue = a.status.hasOwnProperty('succeeded')? a.status.conditions.length - 2: a.status.conditions.length - 1;
-              // console.log(a.status.conditions[indexValue].message);
-              if (a.status?.conditions[indexValue]?.message) {
-                let stringValue2 = a.status?.conditions[indexValue]?.message.split(/\n/)[4];
-                let stringValue = a.status?.conditions[indexValue]?.message.split(/\n/)[3];
-                // let stringValue1 = a.status?.conditions[indexValue]?.message.split(/\n/)[1];
-                // var user =  stringValue1.substring(stringValue1.indexOf(':') + 1).trim();
-                if(stringValue.split(':')[0].trim() == "reportURL"){
-                  var reportId =  stringValue2.substring(stringValue2.indexOf(':') + 1).trim();
-                  if(reportId){
-                    var reportURL = stringValue.substring(stringValue.indexOf(':') + 1).trim() + `&p=${reportId}`;
-                  }
-                // console.log(user);
-                  if(isValidUrl(reportURL)){
-                    setValidUrl(true);
-                    setURL(reportURL);
-                    setLoading(false);
-                    // let b = setInterval(() => {
-                //   let reportPage = document.getElementById("reportPage") as HTMLIFrameElement;
-                //   if(reportPage){
-                //     reportPage.contentWindow.postMessage(user,"*");
-                //     // console.log("IFRAME MSG ", reportPage);
-                //   }
-                // }, 100);
-                // window.addEventListener('message', function(event) {
-                //   if(typeof event.data == 'string' && event.origin != window.location.origin) {
-                //     // console.log(event.data); // Message received from parent
-                //     clearInterval(b);
-                //   }
-                // });
-                //window.open(reportURL, '_blank');
-              
-                  }else{
-                    setValidUrl(false);
-                    setLoading(false);
-                  }
+            let a = JSON.parse(data.manifest);
+          a.status.conditions.filter((conlist: { type: string; }) => conlist.type === 'OpsmxAnalysis').map((element: any) => { conditionArray = [...conditionArray, element] });
+            const latest = conditionArray.reduce(function (r, a) {
+              return r.lastProbeTime > a.lastProbeTime ? r : a;
+            });
+    
+            if(jobsList.length - 1 === lastIterationJob){
+              let stringValue2 = latest.message.split(/\n/)[4];
+              let stringValue = latest.message.split(/\n/)[3];
+              if(stringValue.split(':')[0].trim() == "reportURL"){
+                var reportId = stringValue2.substring(stringValue2.indexOf(':') + 1).trim();
+                if(reportId){
+                  var reportURL = stringValue.substring(stringValue.indexOf(':') + 1).trim() + `&p=${reportId}`;
+                }
+                if(isValidUrl(reportURL)){
+                  setValidUrl(true);
+                  setLoading(false);
+                  setURL(reportURL);
                 }else{
                   setValidUrl(false);
                   setLoading(false);
                 }
-              }
-              
-            }else{
-              setValidUrl(false);
-              setLoading(false);
+              } 
             }
-          }).catch(err => {
-            setValidUrl(false);
-            setLoading(false);
-          });
-      }
-      React.useEffect(() => {
-        { LoadApiCalls(props) }
-      }, []);
+          
+        }).catch(err => {
+        });
+    }
+    React.useEffect(() => {
+      { LoadApiCalls(props) }
+    }, []);
 
-      return (
+    return (
       <WaitFor loading={loading}>
         <div style={{ margin: '1em', width: '100%', height: '100%' }}>
             <div className='bc-element bc-first' style={{ left: '0px' }} onClick={() => props.clickback()}>
